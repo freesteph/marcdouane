20ace9bbcc9fdf1751fa101734a0f7af59acd4e0
Stéphane Maniaci <stephane.maniaci@gmail.com>
2026-06-24 16:12:35 +0200

    bundle gem

---
+++ lib/marcdouane.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "marcdouane/version"
+
+module Marcdouane
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/marcdouane_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Marcdouane do
+  it "has a version number" do
+    expect(Marcdouane::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
